5843c40ff83cf2d4b78436b2043ad9fe3b08e9af
bnned <fluidye@gmail.com>
2015-07-10 02:50:29 -0400

    Worked on Backend
    Added click function and changing html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-var money = 0
+var money = 0;
+var tradeMultiplier = 1;
 
-function click() {
-  
+
+
+
+function trade() {
+    money = money + (1 * tradeMultiplier);
+
+  return money;
+}
+
+function moneyUpdate() {
+  document.getElementById("moneyTotal").innerHTML = "$" + money;
+}
+
+function buy() {
+if (money >= 50) {
+  tradeMultiplier = 2;
+  return tradeMultiplier;
+}
 }
