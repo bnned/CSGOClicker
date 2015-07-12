376ed37eae4593baaf6a9108d8ca59d3d488fa4a
Juan Diego <klassicedits@gmail.com>
2015-07-12 18:49:13 -0400

    v0.01 - CSGO Clicker
    Working timer and manual/auto clicks.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,50 @@
+var moneyPs = 0;
+var moneyPsMultiplier = 1;
 var money = 0;
-var tradeMultiplier = 1;
+var manualMultiplier = 0;
+var fps = 30;
 
+document.getElementById("trade").onclick = manualTrade;
+document.getElementById("buyMarketBot").onclick = function() {
+  if (money >= 100) {
+    money -= 100;
+    moneyPs += 1;
+    updateMoney();
+  }
+};
 
-function trade() {
-    money = money + (1 * tradeMultiplier);
+var items = [marketBot];
 
-    return money;
+var marketBot = {
+  name:"Market Bot",
+  price:1000,
+  description:"Very basic marketbot that you found online for cheap."
+};
+
+function itemCheck() {
+  if (money >= 100) {
+    document.getElementById("buyMarketBot").style.display = 'block';
+  } else if (money < 100) {
+    document.getElementById("buyMarketBot").style.display = 'none';
+  }
 }
 
-function moneyUpdate() {
-  document.getElementById("moneyTotal").innerHTML = "$" + money;
+function manualTrade() {
+  money += 1 + (1 * manualMultiplier);
+  updateMoney();
 }
 
-function buy() {
-    if (money >= 50) {
-        tradeMultiplier = 2;
-        return tradeMultiplier;
-    }
+function moneyGain() {
+  money += (moneyPs * moneyPsMultiplier) / (1000 / fps);
 }
+
+function updateMoney() {
+  document.getElementById("moneyTotal").innerHTML = "$" + money.toFixed(2);
+  document.getElementById("moneyPs").innerHTML = "$" + moneyPs.toFixed(2) + " /s";
+}
+
+setInterval(function() {
+  itemCheck();
+  moneyGain();
+  updateMoney();
+}, 1000 / fps);
