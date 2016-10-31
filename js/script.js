b8fdf5392f07e837d8cc8e10a916a4e084991c6a
bnned <fluidye@gmail.com>
2016-10-31 17:35:04 -0400

    Game Information Object
    Instead of wonky global variables everywhere, they are all neatly
    located in a Game object, which stores settings, player/bot info, and
    generally all settings.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,64 @@
-// CSGOCLICKER 
+// CSGOCLICKER
+
+var Game = {
+  'variables': {
+    'currentCase': 1
+    'rarity': {
+      'milspec': 0.75, // 0 - 0.75
+      'restricted': 0.92, // 0.75 - 0.92
+      'classified': 0.97, // 0.92 - 0.97
+      'covert': 0.995, // 0.97 - 0.995
+    }
+  },
+  'player': {
+    'money': 0,
+    'itemCounter': 0,
+    'maxItems': 50,
+    'items': [],
+    'upgrades': []
+  },
+  'jackpot': {
+    'pot': {
+      'player': {
+        'tickets': 0,
+        'items': []
+      },
+      'bots': {
+        'bot1': {
+          'tickets': 234,
+          'items': []
+        }
+        // 'bot2' and so on, programatically generate, wipe before start of next pot
+      },
+      'potItems': {
+
+      }
+    }
+  },
+  'settings': {
+    'popup': true
+  }
+}
+
+// LOGIC
+
+function randSkin() {
+     var skinsArray = [];
+     var randSkin = "";
+     var randNum = Math.random().toFixed(3); //rounded to 3 places to make it slightly easier to get certain rarities
+     var rarity = "";
+     var identifier;
+
+     if (randNum <= rarityValue.milspec) {
+       rarity = "milspec";
+     } else if (randNum >= rarityValue.milspec && randNum <= rarityValue.restricted) {
+       rarity = "restricted";
+     } else if (randNum >= rarityValue.restricted && randNum <= rarityValue.classified) {
+       rarity = "classified";
+     } else if (randNum >= rarityValue.classified && randNum <= rarityValue.covert) {
+       rarity = "covert";
+     } else {
+       rarity = "knife";
+     }
+
+}
